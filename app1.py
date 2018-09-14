a0224ec192d22808eb881a7ac26661a1c4fd9106
Ben Dover <bdover@gmail.com>
2018-09-14 20:18:34 +0100

    explanation text added

--- app1.py
+++ app1.py
@@ -1,3 +1,28 @@
+'''
+An explanation of the argument 'self' in classes
+-------------------------------------------------
+Many instances can be created from the same class (instantiation)
+    eg instance = class()
+
+to identify the different instances when calling a method on an instance the instance itself is passed as an argument
+to the method... as follows:
+
+            class ex_class():
+                def ex_method(self):
+                    pass
+
+            my_instance = ex_class()
+            my_instance.ex_method(instance)       <--- we dont need to use instance.. this is what python does
+
+instance is effectively passed to self.  However we dont need to specify "instance". it is implied by python.
+However if we dont specify "self" as a parameter in method in a class an error will be produced as follows:
+
+            TypeError: ex_method() takes 0 positional arguments but 1 was given
+
+what follows is a program that demonstrates that "self" = object instance
+'''
+
+
 class Arith():
     w = 100
 
@@ -5,7 +30,7 @@ class Arith():
         self.x = x
         self.y = y
 
-    def addition(self):
+    def addition():
         print(self, "         <------- prints 'self' which is the same as instance above [so self = instance]")
         return self.x + self.y
 
@@ -14,16 +39,24 @@ class Arith():
 
 
 a = Arith(10, 5)
-print(a, "         <------- Instance comprising __name__ + class_name + memory")
+print(a, "         <------- Instance comprising <class_module>.<class_name> + memory")
 print(a.w)
 print(a.addition())
 print(a.subtraction())
 
 b = Arith(10, 5)
-print(b, "         <------- Instance comprising __name__ + class_name + memory")
+print(b, "         <------- Instance comprising <class_module>.<class_name> + memory")
 print(b.w)
 print(b.addition())
 print(b.subtraction())
 
 
+'''
+if class is defined in the __main__ module then <class_module> = __main__
+    eg  <__main__.Arith object at 0x00716850> 
 
+if class is defined in an imported module then <class_module> = module name
+    eg  from app1 import Arith
+        <app1.Arith object at 0x030AEBB0>
+
+'''
